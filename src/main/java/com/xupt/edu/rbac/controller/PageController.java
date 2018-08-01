cc1959fab6843432040244466024b0d2821b95a1
Administrator <zhaowanyue1118@163.com>
2018-08-01 14:23:11 +0800

    fff

--- src/main/java/com/xupt/edu/rbac/controller/PageController.java
+++ src/main/java/com/xupt/edu/rbac/controller/PageController.java
@@ -15,20 +15,25 @@ public class PageController {
 	}
 
 	// @RequiresPermissions("deleteOrder")
-	@RequestMapping("deleteOrder")
-	public String deleteOrder() {
-		return "deleteOrder";
+	@RequestMapping("addStudio")
+	public String addStudio() {
+		return "addStudio";
 	}
 
 	// @RequiresRoles("productManager")
-	@RequestMapping("deleteProduct")
-	public String deleteProduct() {
-		return "deleteProduct";
+	@RequestMapping("deleteStudio")
+	public String deleteStudio() {
+		return "deleteStudio";
 	}
 
-	@RequestMapping("listProduct")
-	public String listProduct() {
-		return "listProduct";
+	@RequestMapping("listStudio")
+	public String listStudio() {
+		return "listStudio";
+	}
+
+	@RequestMapping("updateStudio")
+	public String updateStudio() {
+		return "updateStudio";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -40,5 +45,95 @@ public class PageController {
 	public String noPerms() {
 		return "unauthorized";
 	}
+
+	// @RequiresPermissions("deleteOrder")
+	@RequestMapping("addPlay")
+	public String addPlay() {
+		return "addPlay";
+	}
+
+	// @RequiresRoles("productManager")
+	@RequestMapping("deletePlay")
+	public String deletePlay() {
+		return "deletePlay";
+	}
+
+	@RequestMapping("listPlay")
+	public String listPlay() {
+		return "listPlay";
+	}
+
+	@RequestMapping("updatePlay")
+	public String updatePlay() {
+		return "updatePlay";
+	}
+
+
+
+	@RequestMapping("deleteSchedule")
+	public String deleteSchedule() {
+		return "deleteSchedule";
+	}
+
+
+	@RequestMapping("addSchedule")
+	public String addSchedule() {
+		return "addSchedule";
+	}
+
+	@RequestMapping("listSchedule")
+	public String listSchedule() {
+		return "listSchedule";
+	}
+
+	@RequestMapping("updateSchedule")
+	public String updateSchedule() {
+		return "updateSchedule";
+	}
+
+
+	@RequestMapping("deleteTicket")
+	public String deleteTicket() {
+		return "deleteTicket";
+	}
+
+	@RequestMapping("addTicket")
+	public String addTicket() {
+		return "addTicket";
+	}
+
+	@RequestMapping("updateTicket")
+	public String updateTicket() {
+		return "updateTicket";
+	}
+
+	@RequestMapping("listTicket")
+	public String listTicket() {
+		return "listTicket";
+	}
+
+
+
+	@RequestMapping("sumSaleRecord")
+	public String sumSaleRecord() {
+		return "sumSaleRecord";
+	}
+
+	@RequestMapping("listSaleRecord")
+	public String listSaleRecord() {
+		return "listSaleRecord";
+	}
+
+	@RequestMapping("addSaleItem")
+	public String addSaleItem() {
+		return "addSaleItem";
+	}
+
+	@RequestMapping("listSaleItem")
+	public String listSaleItem() {
+		return "listSaleItem";
+	}
+
+
 
 }
